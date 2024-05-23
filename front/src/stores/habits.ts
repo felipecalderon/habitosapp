d22bfdcdda558dd1859a35250d61ad4d5431d6c1
SantiPagge <santy.pagge@gmail.com>
2024-05-23 18:57:38 -0300

    probando rama

--- front/src/stores/habits.ts
+++ front/src/stores/habits.ts
@@ -2,16 +2,18 @@ import { Habit } from "@/interfaces";
 import { create } from "zustand";
 
 interface HabitStore {
-    habits: Habit[]
-    setHabits: (habits: Habit[]) => void
-    createHabit: (habit: Habit) => void
+  habits: Habit[];
+  setHabits: (habits: Habit[]) => void;
+  createHabit: (habit: Habit) => void;
 }
 
 export const useHabit = create<HabitStore>((set, get) => ({
-    habits: [],
-    setHabits: (habits) => set({habits}),
-    createHabit: (habit) => {
-        const { habits } = get()
-        set({ habits: [...habits, habit] })
-    },
-}))
+  habits: [],
+  setHabits: (habits) => set({ habits }),
+  createHabit: (habit) => {
+    const { habits } = get();
+    set({ habits: [...habits, habit] });
+  },
+}));
+
+// probando rama
